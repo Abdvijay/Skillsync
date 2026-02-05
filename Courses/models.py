95f683ef09a0bd036e5a2dacbcb15224ad6dcfb0
Abdvijay <abdvijay19@gmail.com>
2026-02-05 17:52:08 +0530

    created new app - courses

---
+++ Courses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Course(models.Model):
+
+    course_name = models.CharField(max_length=150)
+    course_code = models.CharField(max_length=10, unique=True)
+
+    description = models.TextField()
+    duration = models.CharField(max_length=50)
+
+    created_by = models.CharField(max_length=100)
+    updated_by = models.CharField(max_length=100, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "courses"
+
+    def __str__(self):
+        return f"{self.course_name} ({self.course_code})"
--- UserDetails/models.py
+++ UserDetails/models.py
@@ -24,24 +24,4 @@ class UserDetails(models.Model):
         db_table = "userdetails"
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
-
-class Course(models.Model):
-
-    course_name = models.CharField(max_length=150) # Python full stack
-    course_code = models.CharField(max_length=10, unique=True) # PFS
-
-    description = models.TextField() # Python, HTML, CSS and etc.
-    duration = models.CharField(max_length=50)   # e.g. "6 Months"
-
-    created_by = models.CharField(max_length=100) # admin
-    updated_by = models.CharField(max_length=100, blank=True, null=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        db_table = "courses"
-
-    def __str__(self):
-        return f"{self.course_name} ({self.course_code})"   
+        return f"{self.username} ({self.role})"  
